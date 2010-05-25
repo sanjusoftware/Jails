3905b1d78b1060675d9503082ec2645c1a51acb0
Sanjeev Kumar Mishra <sanjusoftware@gmail.com>
2010-05-25 11:46:47 +0530

    added CRUD operation test for database

--- database/test/org/jailsframework/database/DatabaseTest.java
+++ database/test/org/jailsframework/database/DatabaseTest.java
@@ -1,6 +1,8 @@
 package org.jailsframework.database;
 
 import junit.framework.Assert;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.ResultSet;
@@ -13,21 +15,53 @@ import java.sql.SQLException;
  *          Time: 12:14:09 AM
  */
 public class DatabaseTest {
+
+    @Before
+    public void setUp() {
+        getDatabase().executeUpdate("TRUNCATE users");
+        getDatabase().executeUpdate("INSERT INTO USERS VALUES ('test')");
+    }
+
+    @After
+    public void tearDown() {
+        getDatabase().executeUpdate("TRUNCATE users");
+    }
+
+
+    @Test
+    public void shouldExecuteAGivenInsertQuery() {
+        getDatabase().executeUpdate("TRUNCATE users");
+        int result = getDatabase().executeUpdate("INSERT INTO USERS VALUES ('sanjeev')");
+        Assert.assertEquals(1, result);
+        assertSelectReturns("sanjeev");
+    }
+
+    @Test
+    public void shouldExecuteAGivenDeleteQuery() {
+        int result = getDatabase().executeUpdate("UPDATE USERS SET name = 'new name' WHERE name = 'test'");
+        Assert.assertEquals(1, result);
+        assertSelectReturns("new name");
+    }
+
     @Test
     public void shouldExecuteAGivenSelectQuery() {
-        ResultSet resultSet = getDatabase().executeQuery("Select * from users");
-        try {
-            resultSet.next();
-            String name = resultSet.getString("name");
-            Assert.assertEquals("sanjeev", name);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        assertSelectReturns("test");
     }
 
     @Test
     public void shouldExecuteAGivenUpdateQuery() {
         Assert.assertTrue(getDatabase().execute("Select * from users"));
+    }
+
+    private void assertSelectReturns(String value) {
+        ResultSet resultSet = getDatabase().executeQuery("Select * from users");
+        try {
+            resultSet.next();
+            String name = resultSet.getString("name");
+            Assert.assertEquals(value, name);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     private IDatabase getDatabase() {
